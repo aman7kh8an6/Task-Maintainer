ff0274d59457caf0aa1658889dfcd7c7d8d72143
aman7kh8an6 <aman7kh8an6@gmail.com>
2021-12-16 01:02:52 +0530

    Uploaded

---
+++ script.js
@@ -0,0 +1,76 @@
+var enterButton = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.getElementById("todo-content");
+var item = document.getElementsByTagName("li");
+var time = document.getElementById("userTime");
+
+var arrTask = new Array();
+var arrTime = new Array();
+
+function inputLength() {
+    return input.value.length;
+}
+
+function listLength() {
+    return item.length;
+}
+
+function createListElement() {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    arrTask.push(input.value);
+    arrTime.push(time.value);
+    input.value = "";
+    li.append(" " + time.value);
+
+    function crossOut() {
+        li.classList.toggle("done");
+    }
+
+    li.addEventListener("click", crossOut);
+
+    var dBtn = document.createElement("button");
+    dBtn.appendChild(document.createTextNode("X"));
+    li.appendChild(dBtn);
+    dBtn.addEventListener("click", deleteListItem);
+
+    function deleteListItem() {
+        li.classList.add("delete")
+    }
+}
+setInterval(writeQuote, 1000);
+function writeQuote() {
+    var picQuote = document.getElementById("quote");
+    var currentTime = new Date();
+    var currtime = currentTime.getHours() + ":"
+        + currentTime.getMinutes();
+    console.log(currtime);
+    var i = 0;
+    while (i < arrTime.length) {
+        if (currtime === arrTime[i]) {
+            picQuote.innerHTML = arrTask[i];
+        }
+        i++;
+    }
+}
+
+
+
+function addListAfterClick() {
+    if (inputLength() > 0) {
+        createListElement();
+    }
+}
+
+function addListAfterKeypress(event) {
+    if (inputLength() > 0 && event.which === 13) {
+        createListElement();
+    }
+}
+
+
+enterButton.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
