3b8fb45437a636262be8ef53d6946e98d0301e10
aman7kh8an6 <42239133+aman7kh8an6@users.noreply.github.com>
2022-12-17 14:59:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ var input = document.getElementById("userinput");
 var ul = document.getElementById("todo-content");
 var item = document.getElementsByTagName("li");
 var time = document.getElementById("userTime");
+var comp_ul = document.getElementById("completed-task");
 
-var arrTask = new Array();
-var arrTime = new Array();
 
 function inputLength() {
     return input.value.length;
@@ -15,30 +14,43 @@ function listLength() {
     return item.length;
 }
 
+
 function createListElement() {
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
-    arrTask.push(input.value);
-    arrTime.push(time.value);
+    
     input.value = "";
-    li.append(" " + time.value);
+   
 
     function crossOut() {
         li.classList.toggle("done");
     }
 
     li.addEventListener("click", crossOut);
-
+    
     var dBtn = document.createElement("button");
     dBtn.appendChild(document.createTextNode("X"));
     li.appendChild(dBtn);
     dBtn.addEventListener("click", deleteListItem);
 
+    let doneButton = document.createElement("button");
+    doneButton.appendChild(document.createTextNode("Done"));
+    li.appendChild(doneButton);
+    doneButton.addEventListener("click", doneListItem);
+
+
+    var tim = document.createElement("h6");
+    tim.appendChild(document.createTextNode(time.value));
+    li.append(tim);
     function deleteListItem() {
         li.classList.add("delete")
     }
+    function doneListItem(){
+        comp_ul.append(li)
+    }
 }
+
 
 function addListAfterClick() {
     if (inputLength() > 0) {
@@ -52,8 +64,6 @@ function addListAfterKeypress(event) {
     }
 }
 
-
 enterButton.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
-
